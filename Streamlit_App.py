4fe0702112d39ded4913df0739beb0bc8b289f91
Hemalakshmi-Git <160298553+Hemalakshmi-Git@users.noreply.github.com>
2024-02-17 10:27:29 +0530

    Update Streamlit_App.py
    PICK LIST

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,7 +1,10 @@
 import streamlit
 import pandas
-streamlit.title('Quotes for the day')
+streamlit.title('Hotel K')
 streamlit.header('Quote1')
 streamlit.text('Good things take time 💻')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
