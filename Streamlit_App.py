e2ed5d5cb08e8ced81898145990dcfd9174df092
Hemalakshmi-Git <160298553+Hemalakshmi-Git@users.noreply.github.com>
2024-02-17 10:10:53 +0530

    Update Streamlit_App.py
    added dataframe to streamlit

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas
 streamlit.title('Quotes for the day')
 streamlit.header('Quote1')
 streamlit.text('Good things take time 💻')
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
